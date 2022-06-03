029dce7818119b972b2910ad2bc4bcd31fae3c9d
EitanHahn <hahne25@greenhill.org>
2022-06-03 10:01:06 -0500

    homework

--- Lists.py
+++ Lists.py
@@ -2,6 +2,7 @@
 #We are going to learn about lists, functions to lists
 #We are going to learn about forloop
 import os
+import random
 os.system('cls')
 
 thislist = ["apple", "banana", "cherry","orange","kiwi","melon","mango"]
@@ -18,7 +19,7 @@ print(thislist[-4:-1])
 if "apple" in thislist:
     print ("yes, the apple is in the list")
 
-for num in range(10):
+for num in range(10): #loops a specific number of times
     print(num,end= " ")
 
 print()
@@ -28,3 +29,30 @@ for element in thislist: #element = thislist (times run through loop)
 
 thislist.append("pinapple") #add an element to the end of the list
 print(thislist[0:])
+
+#for num in range(2):
+    #thislist.append(input("input a food"))
+#print(thislist[0:])
+
+thislist.insert(1,"pinapple") #adding an element to a specific index insert (index, element you want to add)
+print(thislist[0:])
+
+for i in range(len(thislist)):
+    print(thislist[i], end=" / ")
+print()
+
+list_num = [1, 2, 3, 4]
+list_num.extend(thislist) #adds the elements
+print(list_num)
+
+list_num = [1, 2, 3, 4]
+list_num.append(thislist) #adds the whole list to the last index
+print(list_num[-1]) #print the last element
+print(list_num[-1][0]) #print an element in in an element if that element is a list [0,1,2,3,]
+
+word=random.choice(thislist) #picks a random element in the list
+print(word)
+
+guess=input("input a food")
+if guess in word:
+    print("congrats, you guessed the word")
