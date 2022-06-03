363200e3f1e086d578433e1e6ab0f0962b54b09d
EitanHahn <hahne25@greenhill.org>
2022-06-03 10:01:18 -0500

    updated

--- IntegersAndFloats.py
+++ IntegersAndFloats.py
@@ -7,11 +7,15 @@ import os
 os.system('cls')
 variable=input('enter a number') #asking for user's number
 num=int(variable) #making sure number is integer
-variable=(num%2)
-print(num % 2) #using modulus to get the remainder of the number divided by 2
-if variable == 0:
+print(num) #using modulus to get the remainder of the number divided by 2
+if num % 2 == 0:
     print('your number is even')
 else:
     print('your number is odd')
 #is number a multiple of 3?
 
+if (num%3==0):
+    print ('your number is a multiple of 3')
+
+if num%5==0:
+    print("your number is a multiple of 5")
